b77db1b44b3b457c13daadd06d21a0f4a6fef58e
Anna <anjanadvorna@gmail.com>
2024-10-13 23:10:08 +0300

    update BookForm

--- src/components/BookLessonForm/BookLessonForm.jsx
+++ src/components/BookLessonForm/BookLessonForm.jsx
@@ -1,56 +1,116 @@
-import {TeacherAvatar} from './BookLessonForm.styled'
+import { FormInput } from 'components/AuthForm/AuthForm.styled';
+import {
+  TeacherAvatar,
+  FormTitle,
+  FormSummary,
+  TeacherInfoWrapper,
+  TeacherInfo,
+  TeacherNameLable,
+  TeacherName,
+  RadioLegend,
+  RadioList,
+  RadioLabel,
+  RadioInput,
+  RadioItem,
+  TextInputList,
+} from './BookLessonForm.styled';
+import MainButton from 'components/Buttons/MainButton/MainButton';
 
 const BookLessonForm = ({ teacherData }) => {
-    console.log(teacherData)
-    return <form>
-        <b>Book trial lesson</b>
-        <p>Our experienced tutor will assess your current language level, discuss your learning goals, and tailor the lesson to your specific needs.</p>
-        <TeacherAvatar src={teacherData.avatar_url} alt='teacher avatar' />
-        <p>Your Teacher</p>
-        <b>{teacherData.name + ' ' + teacherData.surname}</b>
-        <b>What is your main reason for learning English?</b>
-        <ul>
-            <li> 
-                <label htmlFor='career and business'>
-                    <input type="radio" name='reason' value='career and business' />
-                    Career and business
-                </label>
-                <label htmlFor='lesson for kids'>
-                    <input type="radio" name='reason' value='lesson for kids' />
-                    Lesson for kids
-                </label>
-                <label htmlFor='living abroad'>
-                    <input type="radio" name='reason' value='living abroad' />
-                    Living abroad
-                </label>
-                <label htmlFor='exams and coursework'>
-                    <input type="radio" name='reason' value='exams and coursework' />
-                    Exams and coursework
-                </label>
-                <label htmlFor='culture, travel or hobby'>
-                    <input type="radio" name='reason' value='culture, travel or hobby' />
-                    Culture, travel or hobby
-                </label>                
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <label htmlFor='full name'>
-                    <input type='text' placeholder='Ful Name' name='full name' required/>
-                </label>
-            </li>
-            <li>
-                <label htmlFor='email'>
-                    <input type='email' placeholder='Email' name='email' required/>
-                </label>
-            </li>
-            <li>
-            <label htmlFor='phone'>
-                    <input type='tel' placeholder='Phone Number' name='phone' required/>
-                </label>
-            </li>
-        </ul>
-    </form>
-}
+  console.log(teacherData);
+  return (
+    <form>
+      <FormTitle>Book trial lesson</FormTitle>
+      <FormSummary>
+        Our experienced tutor will assess your current language level, discuss
+        your learning goals, and tailor the lesson to your specific needs.
+      </FormSummary>
 
-export default BookLessonForm; 
+      <TeacherInfoWrapper>
+        <TeacherAvatar src={teacherData.avatar_url} alt="teacher avatar" />
+        <TeacherInfo>
+          <TeacherNameLable>Your Teacher</TeacherNameLable>
+          <TeacherName>
+            {teacherData.name + ' ' + teacherData.surname}
+          </TeacherName>
+        </TeacherInfo>
+      </TeacherInfoWrapper>
+
+      <RadioLegend>What is your main reason for learning English?</RadioLegend>
+      <RadioList>
+        <RadioItem>
+          <RadioLabel htmlFor="career and business">
+            <RadioInput
+              type="radio"
+              name="reason"
+              value="career and business"
+            />
+            Career and business
+          </RadioLabel>
+        </RadioItem>
+        <RadioItem>
+          <RadioLabel htmlFor="lesson for kids">
+            <RadioInput type="radio" name="reason" value="lesson for kids" />
+            Lesson for kids
+          </RadioLabel>
+        </RadioItem>
+        <RadioItem>
+          <RadioLabel htmlFor="living abroad">
+            <RadioInput type="radio" name="reason" value="living abroad" />
+            Living abroad
+          </RadioLabel>
+        </RadioItem>
+        <RadioItem>
+          <RadioLabel htmlFor="exams and coursework">
+            <RadioInput
+              type="radio"
+              name="reason"
+              value="exams and coursework"
+            />
+            Exams and coursework
+          </RadioLabel>
+        </RadioItem>
+        <RadioItem i>
+          <RadioLabel htmlFor="culture, travel or hobby">
+            <RadioInput
+              type="radio"
+              name="reason"
+              value="culture, travel or hobby"
+            />
+            Culture, travel or hobby
+          </RadioLabel>
+        </RadioItem>
+      </RadioList>
+      <TextInputList>
+        <li>
+          <label htmlFor="full name">
+            <FormInput
+              type="text"
+              placeholder="Ful Name"
+              name="full name"
+              required
+            />
+          </label>
+        </li>
+        <li>
+          <label htmlFor="email">
+            <FormInput type="email" placeholder="Email" name="email" required />
+          </label>
+        </li>
+        <li>
+          <label htmlFor="phone">
+            <FormInput
+              type="tel"
+              placeholder="Phone Number"
+              name="phone"
+              required
+            />
+          </label>
+        </li>
+      </TextInputList>
+      <MainButton text="Book" type="submit" />
+    </form>
+  );
+};
+
+export default BookLessonForm;
--- src/components/BookLessonForm/BookLessonForm.styled.jsx
+++ src/components/BookLessonForm/BookLessonForm.styled.jsx
@@ -1,7 +1,109 @@
 import styled from '@emotion/styled';
 
+export const FormTitle = styled.b`
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 1.2;
+  margin-bottom: 20px;
+`;
+
+export const FormSummary = styled.p`
+  font-size: 16px;
+  line-height: 1.375;
+  color: var(--secondary-transparent-black);
+  margin-bottom: 20px;
+`;
+
+export const TeacherInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 40px;
+`;
+
 export const TeacherAvatar = styled.img`
-    width: 44px;
-    height: 44px;
-    border-radius: 100px;
-`
+  width: 44px;
+  height: 44px;
+  border-radius: 100px;
+  margin-right: 14px;
+`;
+
+export const TeacherInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  font-weight: 500;
+`;
+
+export const TeacherNameLable = styled.p`
+  font-size: 12px;
+  line-height: 1.33333;
+  color: var(--secondary-black);
+`;
+
+export const TeacherName = styled.p`
+  font-size: 16px;
+  line-height: 1.5;
+  color: var(--primary-black);
+`;
+
+export const RadioLegend = styled.p`
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 1.33333;
+  color: var(--primary-black);
+  margin-bottom: 20px;
+`;
+
+export const RadioList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  margin-bottom: 40px;
+`;
+
+export const RadioItem = styled.li`
+  display: flex;
+  position: relative;
+  /* padding-left: 32px; */
+  align-items: center;
+`;
+
+export const RadioLabel = styled.label`
+  padding-left: 32px;
+  font-size: 16px;
+  line-height: 1.375;
+`;
+
+export const RadioInput = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 0;
+  width: 0;
+  cursor: pointer;
+
+  &::after {
+    content: '';
+    display: block;
+    width: 24px;
+    height: 24px;
+    border-radius: 100%;
+    background-color: transparent;
+    border: 2px solid rgba(18, 20, 23, 0.278);
+  }
+
+  &:checked::after {
+    background-color: var(--accent);
+    border: 2px solid var(--accent);
+    outline: 4px solid white;
+    outline-offset: -6px;
+  }
+`;
+
+export const TextInputList = styled.ul`
+  margin-bottom: 40px;
+
+  & > li:not(:last-child) {
+    margin-bottom: 18px;
+  }
+`;
